29fe9373bc0844fcc2d36fb8044d0af70ee0b619
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2026-03-08 20:26:12 +0530

    Update shared_client.py

--- shared_client.py
+++ shared_client.py
@@ -1,28 +1,29 @@
-# Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
-# Licensed under the GNU General Public License v3.0.  
-# See LICENSE file in the repository root for full license text.
+# Copyright (c) 2026 TeamJB
+# Repository: https://github.com/teamjb1/teamjassbeniwal-ctrl
+# Licensed under the GNU General Public License v3.0.
 
 from telethon import TelegramClient
 from config import API_ID, API_HASH, BOT_TOKEN, STRING
 from pyrogram import Client
 import sys
 
-client = TelegramClient("telethonbot", API_ID, API_HASH)
+client = TelegramClient("teamjbbot", API_ID, API_HASH)
 app = Client("pyrogrambot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 userbot = Client("4gbbot", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
 
 async def start_client():
     if not client.is_connected():
         await client.start(bot_token=BOT_TOKEN)
-        print("SpyLib started...")
+        print("TeamJB Library Started...")
+
     if STRING:
         try:
             await userbot.start()
             print("Userbot started...")
         except Exception as e:
-            print(f"Hey honey!! check your premium string session, it may be invalid of expire {e}")
+            print(f"Session error: please check your premium string session. {e}")
             sys.exit(1)
-    await app.start()
-    print("Pyro App Started...")
-    return client, app, userbot
 
+    await app.start()
+    print("Pyrogram App Started...")
+    return client, app, userbot
