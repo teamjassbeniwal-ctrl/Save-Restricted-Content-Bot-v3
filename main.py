b17abc3e0769751b8e68b43844ddfd5698a611f8
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2026-03-08 20:24:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
-# Licensed under the GNU General Public License v3.0.  
-# See LICENSE file in the repository root for full license text.
+# Copyright (c) 2026 TeamJB
+# Repository: https://github.com/teamjb1/teamjassbeniwal-ctrl
+# Licensed under the GNU General Public License v3.0.
 
 import asyncio
 from shared_client import start_client
@@ -17,16 +17,16 @@ async def load_and_run_plugins():
         module = importlib.import_module(f"plugins.{plugin}")
         if hasattr(module, f"run_{plugin}_plugin"):
             print(f"Running {plugin} plugin...")
-            await getattr(module, f"run_{plugin}_plugin")()  
+            await getattr(module, f"run_{plugin}_plugin")()
 
 async def main():
     await load_and_run_plugins()
     while True:
-        await asyncio.sleep(1)  
+        await asyncio.sleep(1)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    print("Starting clients ...")
+    print("Starting TeamJB Bot Clients ...")
     try:
         loop.run_until_complete(main())
     except KeyboardInterrupt:
